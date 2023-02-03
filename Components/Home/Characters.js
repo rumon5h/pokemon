84107336fba9d50dc9bd3ba74048fdc80daecaa0
rumon5h <mdrumonkhan48519@gmail.com>
2023-02-03 18:12:07 +0600

    title added

--- Components/Home/Characters.js
+++ Components/Home/Characters.js
@@ -3,15 +3,24 @@ import React from 'react'
 import styles from '../../src/styles/Characters.module.css';
 
 const Characters = () => {
+
     return (
         <div>
             <div className={styles.bg}>
                 <div className='mx-auto w-[fit-content] pt-[64px]'>
-                    <Image src='/images/Logo.png' width={265} height={96}></Image>
+                    <Image src='/images/Logo.png' alt='logo' priority width={265} height={96}></Image>
                 </div>
-               
+                <div>
+                    {
+
+                    }
+                </div>
             </div>
             <div>
+                <div className={styles.brush}>
+                    <h3 className='text-5xl font-bold text-center'>Ash & Pikachu Arrive in
+                        Pokémon Universe</h3>
+                </div>
                
             </div>
         </div>
@@ -19,3 +28,5 @@ const Characters = () => {
 }
 
 export default Characters;
+
+
