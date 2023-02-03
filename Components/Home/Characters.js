d302a56eb379e69b20a6482954c3c306d9c2ea87
rumon5h <mdrumonkhan48519@gmail.com>
2023-02-03 16:07:52 +0600

    background image added

--- Components/Home/Characters.js
+++ Components/Home/Characters.js
@@ -1,9 +1,16 @@
+import Image from 'next/image';
 import React from 'react'
+import styles from '../../src/styles/Characters.module.css';
 
 const Characters = () => {
     return (
         <div>
-            
+            <div className={styles.bg}>
+               
+            </div>
+            <div>
+               
+            </div>
         </div>
     )
 }
